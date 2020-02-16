8d066ec41c8d729628da6853b164dda12e39a0b5
patrikainen <patrikainen.externe@osmos-group.com>
2020-02-16 23:43:58 +0100

    basis

---
+++ helpers.js
@@ -0,0 +1,7 @@
+function openNavMenu() {
+  document.getElementById("mobile-nav-menu").classList.add("displayed");
+}
+
+function closeNavMenu() {
+  document.getElementById("mobile-nav-menu").classList.remove("displayed");
+}
